831041d7c4592156bbc37d7440ed28f32133e965
Samuel Wenceslau Lorena <48766835+SamukaWenceslau@users.noreply.github.com>
2020-12-13 12:34:27 -0300

    adjusting layout and correcting date

--- helpers/getDate.js
+++ helpers/getDate.js
@@ -1,12 +1,17 @@
 const getDate = () => {
     const d = new Date();
     
-    const date = d.toLocaleDateString("pt-BR");
+    //const date = d.toLocaleDateString('pt-BR');
+    const dd = d.getDate();
+    const mm = d.getMonth() + 1;
+    const yyyy = d.getFullYear();
+
+    
     const hr = d.getHours()<10?`0${d.getHours()}`: d.getHours();
     const min = d.getMinutes()<10?`0${d.getMinutes()}`: d.getMinutes();
     
     
-    return(`${date} às ${hr}h${min} - Última alteração`);
+    return(`${dd}/${mm}/${yyyy} às ${hr}h${min} - Última alteração`);
 }
 
 module.exports = getDate;
